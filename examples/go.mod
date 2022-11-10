3402cae79c0bc6709154a4d97476bc1ab454e0a7
philip-ellis-sp <philip.ellis@sailpoint.com>
2022-11-10 09:21:43 -0500

    created

---
+++ examples/go.mod
@@ -0,0 +1,16 @@
+module sdk
+
+go 1.17
+
+require sailpointsdk v1.0.0
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/philip-ellis-sp/go-sdk v0.0.0-20221104154732-be7bdf9800de // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/oauth2 v0.1.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
+
+replace sailpointsdk v1.0.0 => ../sdk-output/v3
---
+++ examples/sdk.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	sailpointsdk "github.com/philip-ellis-sp/go-sdk"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("Hello, World1!")
+
+	auth := context.WithValue(context.Background(), sailpointsdk.ContextAccessToken, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRfaWQiOiIyZjlkOTZiMC03ZWIwLTRlNGUtYWE0MS03Nzk2YmU1ODQ2Y2YiLCJwb2QiOiJzdGcwMy11c2Vhc3QxIiwic3Ryb25nX2F1dGhfc3VwcG9ydGVkIjpmYWxzZSwib3JnIjoiZGV2cmVsIiwiaWRlbnRpdHlfaWQiOiIyYzkxODA4OTdkMmNiODBiMDE3ZDM5Y2NiMjZjMTgwNCIsInVzZXJfbmFtZSI6InBoaWxpcC5lbGxpcyIsInNjb3BlIjpbIkJnPT0iXSwic3Ryb25nX2F1dGgiOnRydWUsImV4cCI6MTY2NzYxMjk3NiwiYXV0aG9yaXRpZXMiOlsiT1JHX0FETUlOIiwic3A6dXNlciJdLCJqdGkiOiJUdTB5aEg2a2EwRThzeThjUFY3dVlOdTA2UEEiLCJjbGllbnRfaWQiOiJkNDFiN2QyMDdjMzU0Nzg4OGE2YmZmMWM4ZTExZTE1NCJ9.xuC5iOsv9nRCUjPHy4aPzg5wWoxb--AYGeo57SUxV9M")
+
+	configuration := sailpointsdk.NewConfiguration("devrel")
+
+	apiClient := sailpointsdk.NewAPIClient(configuration)
+
+	resp, r, err := apiClient.AccountsApi.ListAccounts(auth).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `AccountsApi.ListAccounts``: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+	// response from `ListAccounts`: []Account
+	fmt.Fprintf(os.Stdout, "Response from `AccountsApi.ListAccounts`: %v\n", resp)
+
+}
