09e592e5db1efbe2f8db97b5b5f2854fc4cb98ca
philip-ellis-sp <philip.ellis@sailpoint.com>
2022-11-11 16:39:00 -0500

    added client credentials to example

--- examples/sdk.go
+++ examples/sdk.go
@@ -6,13 +6,14 @@ import (
 	"os"
 
 	sailpoint "github.com/philip-ellis-sp/golang-sdk/sdk-output"
+	sailpointsdk "github.com/philip-ellis-sp/golang-sdk/sdk-output/v3"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("Hello, World1!")
 
-	auth := context.WithValue(context.Background(), sailpoint.ContextAccessToken, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRfaWQiOiIyZjlkOTZiMC03ZWIwLTRlNGUtYWE0MS03Nzk2YmU1ODQ2Y2YiLCJwb2QiOiJzdGcwMy11c2Vhc3QxIiwic3Ryb25nX2F1dGhfc3VwcG9ydGVkIjpmYWxzZSwib3JnIjoiZGV2cmVsIiwiaWRlbnRpdHlfaWQiOiIyYzkxODA4OTdkMmNiODBiMDE3ZDM5Y2NiMjZjMTgwNCIsInVzZXJfbmFtZSI6InBoaWxpcC5lbGxpcyIsInNjb3BlIjpbIkJnPT0iXSwic3Ryb25nX2F1dGgiOnRydWUsImV4cCI6MTY2ODE0OTg1NSwiYXV0aG9yaXRpZXMiOlsiT1JHX0FETUlOIiwic3A6dXNlciJdLCJqdGkiOiJkeldoajM0RGxyOEF6eGNQaGFzQmc3U2pBSTQiLCJjbGllbnRfaWQiOiJkNDFiN2QyMDdjMzU0Nzg4OGE2YmZmMWM4ZTExZTE1NCJ9.TPyW40RxX-6h94PMSdfIGBj9SlA-8KtcEgg2OJiE9-g")
+	auth := context.WithValue(context.Background(), sailpoint.ContextClientCredentials, sailpointsdk.ClientCredentials{ClientId: "d41b7d207c3547888a6bff1c8e11e154", ClientSecret: "777fef7e68f9c35ad3eb78e50633028ff4d870b60e6ab1252c0e7bda3ea19ff6"})
 
 	configuration := sailpoint.NewConfiguration("devrel")
 
